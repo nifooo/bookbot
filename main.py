648828b2b27ccd69d368f946a2ba04d245802c3c
nifooo <nifo2013@gmx.de>
2025-11-11 12:23:27 +0100

    word counting in main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-
-
-
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
@@ -9,6 +6,8 @@ def get_book_text(path):
 def main():
     path = "books/frankenstein.txt"
     text_book = get_book_text(path)
-    print(text_book)
+    words = text_book.split()
+    num_words = len(words)
+    print(f"Found {num_words} total words.")
 
 main()
