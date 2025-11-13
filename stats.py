1a226e8b9fd956f2e6f89608a07498a9b53469db
nifooo <nifo2013@gmx.de>
2025-11-13 21:18:20 +0100

    counter added

--- stats.py
+++ stats.py
@@ -1,6 +1,14 @@
 def get_book_text(path):
     with open(path) as f:
         text_book = f.read()
-    words = text_book.split()
-    num_words = len(words)
-    print(f"Found {num_words} total words.")
+    
+    counts = {}
+    for ch in text_book.lower():
+        if ch in counts:
+            counts[ch] += 1
+        else:
+            counts[ch] = 1
+    return print(counts)
+    # words = text_book.split()
+    # num_words = len(words)
+    # return print(f"Found {num_words} total words.")
