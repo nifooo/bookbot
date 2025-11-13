b673c1498287430cc6ee8bbe0e405c12f3baa4d7
nifooo <nifo2013@gmx.de>
2025-11-13 21:01:46 +0100

    reworked stats.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from stats import get_book_text
 
 def main():
     path = "books/frankenstein.txt"
-    text_book = get_book_text(path)
-    words = text_book.split()
-    num_words = len(words)
-    print(f"Found {num_words} total words.")
+    get_book_text(path)
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,6 @@
 def get_book_text(path):
     with open(path) as f:
-        file_contents = f.read()
-    return file_contents
+        text_book = f.read()
+    words = text_book.split()
+    num_words = len(words)
+    print(f"Found {num_words} total words.")
